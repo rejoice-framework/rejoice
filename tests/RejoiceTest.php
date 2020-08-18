3c930cbc889f57ef64c82dbbbc97c955f54d910d
CinPer <damien.dorcis@yahoo.com>
2020-08-18 04:06:20 +0000

    Fix env loading in RejoiceTest test case

--- tests/RejoiceTest.php
+++ tests/RejoiceTest.php
@@ -6,9 +6,16 @@ use PHPUnit\Framework\TestCase;
 use Prinx\Config;
 use Rejoice\Simulator\Libs\Simulator;
 use function Prinx\Dotenv\env;
+use function Prinx\Dotenv\loadEnv;
 
 class RejoiceTest extends TestCase
 {
+    public function __construct()
+    {
+        parent::__construct();
+        loadEnv(realpath(__DIR__.'/../../.env.example'));
+    }
+
     public function testRequest()
     {
         $simulator = new Simulator;
