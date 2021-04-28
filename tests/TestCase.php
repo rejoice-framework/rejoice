81bb19b8ab9d351e7c60840113327cdd5d37ead9
Prince Dorcis <damien.dorcis@yahoo.com>
2021-04-28 08:13:36 +0000

    Modify phpunit.xml

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,8 @@
 
 namespace Tests;
 
-use Illuminate\Database\Capsule\Manager as Capsule;
 use PHPUnit\Framework\TestCase as BaseTestCase;
-use function Prinx\Dotenv\loadEnv;
-use Rejoice\Foundation\Path;
+use Rejoice\Support\Eloquent;
 
 /**
  * Base Test case.
@@ -15,26 +13,6 @@ abstract class TestCase extends BaseTestCase
     public function __construct()
     {
         parent::__construct();
-        loadEnv(Path::toProject('.env'));
-        $this->loadDbConnection();
-    }
-
-    public function loadDbConnection()
-    {
-        $capsule = new Capsule;
-
-        $databasConfig = require Path::toConfig('database.php');
-        $connections = $databasConfig['connections'];
-
-        if ($defaultConnection = $connections[$databasConfig['default']] ?? []) {
-            $capsule->addConnection($defaultConnection);
-        }
-
-        foreach ($connections as $name => $config) {
-            $capsule->addConnection($config, $name);
-        }
-
-        $capsule->bootEloquent();
-        $capsule->setAsGlobal();
+        Eloquent::connect('test');
     }
 }
